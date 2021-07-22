35426e10c390257d5438bffa4412e22e98cc7dd3
Asa Grace <asag182@gmail.com>
2021-07-22 13:54:33 -0600

    login and logout pages

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,13 +59,18 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+        .then(dbUserData =>  {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
         });
 });
 
+// login route
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
@@ -84,8 +89,25 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
+});
+
+// logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 router.put('/:id', (req, res) => {
